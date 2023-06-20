02eaeedf267728a574979a25e728d8bfc37d2718
salvomc <smcrwrk@gmail.com>
2023-06-20 15:57:55 +0200

    base commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  
+  <div>
+    <AppHeader></AppHeader>
+    <AppMain></AppMain>
+    <AppFooter></AppFooter> 
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
